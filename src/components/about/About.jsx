e71df865718c546bd2847a24ba0c8779c0ae111a
msi1995 <lloyddo@oregonstate.edu>
2022-03-31 17:50:35 -0700

    Good functionality first 2 pages mobile and desktop

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,51 +5,64 @@ import Deadlift from "../../images/deadlift2.jpg"
 import DownChevron from "../../svg/DownChevron";
 
 const chevronwrap = css`
-    display: flex;
-    margin-left: 150px;
-    width: 0vh;
-    height: 97vh;
+    height: 5vh;
+    width: 100%;
     animation: bounce 4s ease-in-out infinite;
 
     @media screen and (max-width: 391px){
-        position: absolute;
-        height: 100vh;
-        align-items: end;
-        display: none;
+
     }
+`
+
+
+const test = css`
+position: absolute;
+right: 50%;
+bottom: 0;
+
+@media screen and (max-width: 391px){
+        right: 44%;
+    }
+
 `
 
 function About(){
 
     return(
-
-    <div id="about" className="container">
-        <div className="container-left">
-            <div className="card bg"></div>
-            <div className="card">
-                <img src={Deadlift} 
-                alt="Me deadlifting!" 
-                className="card-img" />
+    <div id="about" className="full-container">
+        <div className="container">
+            <div className="container-left">
+                <div className="card bg"></div>
+                <div className="card">
+                    <img src={Deadlift} 
+                    alt="Me deadlifting!" 
+                    className="card-img" />
+                </div>
+                {/* <div css={chevronwrap}>
+                    <a href="#contact">
+                        <DownChevron/>
+                    </a>
+                </div> */}
             </div>
-            <div css={chevronwrap}>
-                <a href="#contact">
-                    <DownChevron/>
-                </a>
+            <div className="container-right">
+                <h1 className="title">About Me</h1>
+                <p className="description">
+                    I'm finishing up my last year at Oregon State while also working as a developer for the University.
+                    I use Python more than any other language at work, either on its own or alongside some data processing 
+                    frameworks like Pandas when needed.
+                </p>
+                <p className="description">
+                When I'm not in class or working, fitness and especially lifting are very important to me. 
+                    I place a lot of value on staying in shape (even though I hate doing cardio) 
+                    and may even attempt to compete at a powerlifting meet in the future!
+                </p>
+                    
             </div>
         </div>
-        <div className="container-right">
-            <h1 className="title">About Me</h1>
-            <p className="description">
-                I'm finishing up my last year at Oregon State while also working as a developer for the University.
-                I use Python more than any other language at work, either on its own or alongside some data processing 
-                frameworks like Pandas when needed.
-            </p>
-            <p className="description">
-            When I'm not in class or working, fitness and especially lifting are very important to me. 
-                I place a lot of value on staying in shape (even though I hate doing cardio) 
-                and may even attempt to compete at a powerlifting meet in the future!
-            </p>
-                
+        <div css={chevronwrap}>
+            <a css={test} href="#contact">
+                <DownChevron/>
+            </a>
         </div>
     </div>
     )
